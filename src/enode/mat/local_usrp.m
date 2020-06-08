8378904ad6f6a6d9f91888f0d3c891cfb8a2f8a4
Jacob Holtom <jacob@holtom.me>
2020-06-08 15:26:09 -0700

    Add more up to date version of local_usrp.m

--- src/enode/mat/local_usrp.m
+++ src/enode/mat/local_usrp.m
@@ -1,57 +1,81 @@
 classdef local_usrp
-methods (Static)
+    properties
 
-%===========================================================
-function set_usrp(type, ant, subdev, ref, wirefmt, num_samps, ...
-                  sample_rate, freq, rx_gain, tx_gain, bw, setup_time) 
-    fprintf('Conneting to local host, txport 9940\n');
-    local_usrp_mex('txinit', '127.0.0.1', 9940); 
-    fprintf('Conneting to local host, rxport 9942, 9943\n');
-    local_usrp_mex('rxinit', '127.0.0.1', 9942, 9943); 
-end
+        request_num_samps
 
-%===========================================================
-function tx_usrp(start_time, tx_buff)
-    tbuf = single(transpose(tx_buff));
-    fprintf('tx_usrp(), start_time: %f, len=%d\n', start_time, length(tbuf));
-    local_usrp_mex('write', tbuf, start_time);
-end
-
-%===========================================================
-function rx_buff = rx_usrp(start_time)
-
-%fprintf('local_usrp: rx_usrp(), start_time = %f\n', start_time);
-
-    % setup
-    rx_comp_unit = 1000;
-    rx_short_unit = rx_comp_unit * 2;
-
-    % data read
-    local_usrp_mex('rcon', start_time);
-
-    [len, rdat] = local_usrp_mex('read', rx_short_unit);
-    rxdat = transpose(rdat);
-    totalRx = len;
-
-    if(len > 0)
-        while(1)
-            [len, rdat] = local_usrp_mex('read', rx_short_unit);
-            if(len == 0) break; end
-            totalRx = totalRx + len;
-            rxdat = [rxdat transpose(rdat)];
-        end
     end
-    rx_buff = double(transpose(rxdat)) / 2^11;
 
-fprintf('local_usrp: read packet %d complex samples at time %f\n', length(rx_buff)/2, start_time);
-%fprintf('R');
 
-end
 
-%===========================================================
-function terminate_usrp()
-    local_usrp_lib.sys_close();
-end
+    methods (Static)
+        function lId = logicalId()
+            fId = fopen('logicId', 'r');
+            lId = fscanf(fId, '%d');
+            fprintf('node logical ID = %d\n', lId);
+            fclose(fId);
+        end
 
-end
+    end
+
+    methods
+        %=================================================================
+        %===========================================================
+        function this = set_usrp(this,type, ant, subdev, ref, wirefmt, num_samps, ...
+                sample_rate, freq, rx_gain, tx_gain, bw, setup_time)
+            fprintf('Connecting to local host, txport 9940\n');
+            local_usrp_mex('txinit', '127.0.0.1', 9940);
+            fprintf('Connecting to local host, rxport 9942, 9943\n');
+            local_usrp_mex('rxinit', '127.0.0.1', 9944, 9945);
+            this.request_num_samps = num_samps;
+        end
+
+        %===========================================================
+        function tx_usrp(this,start_time, tx_buff)
+            tbuf = single(transpose(tx_buff));
+            fprintf('tx_usrp(), start_time: %f, len=%d\n', start_time, length(tbuf));
+            local_usrp_mex('write', tbuf, start_time);
+        end
+        %===========================================================
+        function rx_buff = rx_usrp(this,start_time)
+
+            fprintf('local_usrp: rx_usrp(), start_time = %f\n', start_time);
+            % setup
+            rx_comp_unit =  this.request_num_samps;
+            %rx_comp_unit = this.request_num_samps;
+
+            rx_short_unit = rx_comp_unit * 2;
+
+            % data read
+            % local_usrp_mex('rcon', start_time);
+
+            %fprintf('rx_usrp(), start_time: %f, len=%f\n', start_time, rx_comp_unit);
+            [len, rdat] = local_usrp_mex('read',rx_short_unit,start_time);
+            rxdat = transpose(rdat);
+            %             totalRx = len;
+
+            %             if(len > 0)
+            %                 while(1)
+            %                     [len, rdat] = local_usrp_mex('read', rx_short_unit);
+            %                     if(len == 0) break; end
+            %                     totalRx = totalRx + len;
+            %                     rxdat = [rxdat transpose(rdat)];
+            %                 end
+            %             end
+
+
+            rx_buff = double(rxdat) / 2^15;
+
+           fprintf('local_usrp: read packet %d complex samples at time %f\n', len, start_time);
+            %fprintf('R');
+
+        end
+
+        %===========================================================
+        function terminate_usrp(this)
+            pause(1);
+            fprintf('Close all resources for USRP control\n');
+            local_usrp_mex('close');
+        end
+
+    end
 end
